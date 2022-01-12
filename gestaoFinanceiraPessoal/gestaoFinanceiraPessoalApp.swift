eb073aa39d987183657e88da574d6bcd525cf05d
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-01-12 11:06:29 -0300

    Initial Commit

---
+++ gestaoFinanceiraPessoal/gestaoFinanceiraPessoalApp.swift
@@ -0,0 +1,20 @@
+//
+//  gestaoFinanceiraPessoalApp.swift
+//  gestaoFinanceiraPessoal
+//
+//  Created by Raphael Alkamim on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct gestaoFinanceiraPessoalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
